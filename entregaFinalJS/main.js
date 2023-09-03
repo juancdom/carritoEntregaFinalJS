f98a642a89c661ba0ca1e842568040e93cfd59c9
juancdom <129569316+juancdom@users.noreply.github.com>
2023-09-03 12:52:58 -0300

    version 1.01

---
+++ entregaFinalJS/main.js
@@ -0,0 +1,21 @@
+// PRODUCTOS
+const productos = [
+    //abrigos
+    {
+        id:"camperon",
+        titulo: "camiseta titular "
+        imagen: "./imagenes/camperon.jpg"
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+
+
+
+
+
+
+
+    }
+]
